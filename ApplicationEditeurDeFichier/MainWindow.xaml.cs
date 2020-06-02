3525b803773af7323766ae6813119e7b58b24152
AxelRigal <37696088+AxelRigal@users.noreply.github.com>
2020-06-02 10:34:37 +0200

    first commit

---
+++ ApplicationEditeurDeFichier/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ApplicationEditeurDeFichier
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private string filepath { get; set; }
+
+
+        private void NewFile(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "Document"; // Default file name
+            dlg.DefaultExt = ".txt"; // Default file extension
+            dlg.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
+                               // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                filepath = dlg.FileName;
+                editorFile.Text = System.IO.File.ReadAllText(filepath);
+            }
+            
+        }
+
+        private void SaveFile(object sender, RoutedEventArgs e)
+        {
+            if(filepath == null)
+            {
+                Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+                dlg.FileName = "Document"; // Default file name
+                dlg.DefaultExt = ".text"; // Default file extension
+                dlg.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
+                                                            // Show save file dialog box
+                Nullable<bool> result = dlg.ShowDialog();
+                // Process save file dialog box results
+                if (result == true)
+                {
+                    // Save document
+                    string filename = dlg.FileName;
+                    System.IO.File.WriteAllText(filename, editorFile.Text);
+                }
+                
+            }
+            else
+            {
+                System.IO.File.WriteAllText(filepath, editorFile.Text);
+            }
+        }
+    }
+}
